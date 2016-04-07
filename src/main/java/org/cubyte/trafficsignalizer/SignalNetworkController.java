2d53e57fefec6ea7b50d44386f822b393a2adccd
Phillip Schichtel <phillip.public@schich.tel>
2016-04-07 15:15:47 +0200

    fixed the group creation and cleaned up the controllers

--- src/main/java/org/cubyte/trafficsignalizer/SignalNetworkController.java
+++ src/main/java/org/cubyte/trafficsignalizer/SignalNetworkController.java
@@ -3,12 +3,19 @@ package org.cubyte.trafficsignalizer;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import org.cubyte.trafficsignalizer.stress.StressFunction;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Network;
+import org.matsim.contrib.signals.events.SignalGroupStateChangedEvent;
+import org.matsim.contrib.signals.events.SignalGroupStateChangedEventHandler;
 import org.matsim.contrib.signals.model.Signal;
+import org.matsim.contrib.signals.model.SignalGroup;
 import org.matsim.contrib.signals.model.SignalSystem;
+import org.matsim.core.api.experimental.events.EventsManager;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static java.util.stream.Collectors.toList;
 
@@ -19,10 +26,38 @@ public class SignalNetworkController {
     private final StressFunction stressFunction;
 
     @Inject
-    public SignalNetworkController(Network network, StressFunction stressFunction) {
+    public SignalNetworkController(Network network, StressFunction stressFunction, EventsManager em) {
         this.network = network;
         this.stressFunction = stressFunction;
         this.controllers = new ArrayList<>();
+        em.addHandler(new SignalGroupStateChangedEventHandler() {
+            private final Map<Id<SignalSystem>, SignalizerController> cache = new HashMap<>();
+
+            @Override
+            public void handleEvent(SignalGroupStateChangedEvent event) {
+                System.out.println("state change!");
+                final Id<SignalSystem> system = event.getSignalSystemId();
+                SignalizerController ctrl = cache.get(system);
+                if (ctrl == null) {
+                    for (SignalizerController c : controllers) {
+                        final SignalSystem s = c.getSystem();
+                        if (s != null && s.getId().equals(system)) {
+                            cache.put(system, c);
+                            ctrl = c;
+                            break;
+                        }
+                    }
+                }
+                if (ctrl != null) {
+                    ctrl.groupStateChanged(event.getSignalGroupId(), event.getNewState());
+                }
+            }
+
+            @Override
+            public void reset(int iteration) {
+                this.cache.clear();
+            }
+        });
     }
 
     public void addController(SignalizerController c) {
@@ -41,7 +76,7 @@ public class SignalNetworkController {
 
     }
 
-    public void receivedSystem(SignalizerController signalizerController, SignalSystem system) {
+    public void controllerReady(SignalizerController signalizerController, SignalSystem system) {
     }
 
 
